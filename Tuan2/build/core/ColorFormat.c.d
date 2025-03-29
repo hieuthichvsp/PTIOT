9f07e8b10599eeefbeb496b8895710083ad0a3ab
VPS <lolhieudzlol@gmail.com>
2025-03-29 08:51:30 +0700

    iot

---
+++ Tuan2/build/core/ColorFormat.c.d
@@ -0,0 +1,3 @@
+c:\Users\lolhi\Documents\Vsc_arduino\PTIOT\Tuan2\build\core\ColorFormat.c.o: \
+ C:\Users\lolhi\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.1.1\cores\esp32\ColorFormat.c \
+ C:\Users\lolhi\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.1.1\cores\esp32\ColorFormat.h
---
+++ Tuan2/build/core/MacAddress.cpp.d
@@ -0,0 +1,9 @@
+c:\Users\lolhi\Documents\Vsc_arduino\PTIOT\Tuan2\build\core\MacAddress.cpp.o: \
+ C:\Users\lolhi\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.1.1\cores\esp32\MacAddress.cpp \
+ C:\Users\lolhi\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.1.1\cores\esp32/MacAddress.h \
+ C:\Users\lolhi\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.1.1\cores\esp32/WString.h \
+ C:\Users\lolhi\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.1.1\cores\esp32/pgmspace.h \
+ C:\Users\lolhi\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.1.1\cores\esp32/Printable.h \
+ C:\Users\lolhi\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.1.1\cores\esp32/Print.h \
+ C:\Users\lolhi\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.1.1\cores\esp32/WString.h \
+ C:\Users\lolhi\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.1.1\cores\esp32/Printable.h
